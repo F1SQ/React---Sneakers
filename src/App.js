5af816fad43a52dd96a8ecae4660cb560a0075de
F1SQ <89566584+F1SQ@users.noreply.github.com>
2022-07-05 15:44:54 +0300

    change title (test commits)

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ function App() {
     return (
         <div className="App">
             <h1>new project will be here!!!!</h1>
-            <h1>new project will be here!!!!</h1>
-            <h1>new project will be here!!!!</h1>
         </div>
     );
 }
